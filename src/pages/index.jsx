721c01b7bc9b21cad7b9a69e6d77b2961d86c362
astabidasaaa <astabidasabcd@gmail.com>
2021-12-22 18:14:27 +0700

    adding SSR checker to homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,30 +1,44 @@
 import * as React from "react";
 import TypeWriterEffect from "react-typewriter-effect";
 import styled from "styled-components";
+import useIsSsr from "../components/IsSSR";
 
 const WelcomeTextStyle = styled.section`
   width: fit-content;
   padding: 6rem 0;
   margin: auto;
+
+  #home-placeholder {
+    // color: var(--black);
+    opacity: 0;
+  }
 `;
 
 const IndexPage = () => {
+  const isSSR = useIsSsr();
   return (
     <>
       <WelcomeTextStyle>
-        <TypeWriterEffect
-          startDelay={1000}
-          cursorColor="white"
-          multiText={[
-            "Hello world!",
-            "I am Sangkara",
-            "Welcome to my portfolio website",
-            "Fonts can be customized.",
-            "Feel free to take a look!",
-          ]}
-          multiTextDelay={1000}
-          typeSpeed={120}
-        />
+        {isSSR ? (
+          <h1 id="home-placeholder">
+            Hello world! I am Sangkara Welcome to my portfolio website. Fonts
+            can be customized. Feel free to take a look!
+          </h1>
+        ) : (
+          <TypeWriterEffect
+            startDelay={1000}
+            cursorColor="white"
+            multiText={[
+              "Hello world!",
+              "I am Sangkara.",
+              "Welcome to my portfolio website.",
+              "Fonts can be customized.",
+              "Feel free to take a look!",
+            ]}
+            multiTextDelay={1000}
+            typeSpeed={120}
+          />
+        )}
       </WelcomeTextStyle>
     </>
   );
