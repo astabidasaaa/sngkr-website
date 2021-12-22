c74711a6b6a22ba94a4d1f3f19bd840018474a49
astabidasaaa <astabidasabcd@gmail.com>
2021-12-22 17:44:11 +0700

    replace website icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/sngkr_smiley_logo.png",
       },
     },
     "gatsby-transformer-remark",
