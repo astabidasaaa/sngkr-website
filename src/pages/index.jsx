e248e2db2a09b0a1ec5784ac00d3e3dd3cb3ecfe
astabidasaaa <astabidasabcd@gmail.com>
2021-12-24 18:12:08 +0700

    update typeeffect plugin and component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,46 +1,82 @@
-import * as React from "react";
-import TypeWriterEffect from "react-typewriter-effect";
+import React, { useEffect, useRef } from "react";
+// import TypeWriterEffect from "react-typewriter-effect";
 import styled from "styled-components";
 import useIsSsr from "../components/IsSSR";
+import Typed from "typed.js";
+import keypressSound from "../sounds/keypress.wav";
 
-const WelcomeTextStyle = styled.section`
-  width: fit-content;
+const WelcomeTextStyle = styled.div`
+  width: 100%;
   padding: 6rem 0;
   margin: auto;
 
   #home-placeholder {
-    // color: var(--black);
-    opacity: 0;
+    color: var(--black);
+    font-size: var(--h6);
+  }
+
+  span {
+    font-size: var(--h1);
+    font: var(--regularWeight) clamp(2rem, 1rem + 5vw, 5.5rem) var(--sansSerif);
+    color: var(--white);
   }
 `;
 
 const IndexPage = () => {
   const isSSR = useIsSsr();
-  return (
-    <>
+  const el = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(".typed1", {
+      strings: [
+        "Hello wprdl",
+        "Hello world!",
+        "Hello world!",
+        "Hello world!",
+        "I am <strong>Sangkara</strong>.",
+        "I am <strong>Sangkara</strong>.",
+        "I am <strong>Sangkara</strong>.",
+        "Welcime",
+        "Welcome ot",
+        "Welcome to my portfolio weebsi",
+        "Welcome to my portfolio website.",
+        "Welcome to my portfolio website.",
+        "Welcome to my portfolio website.",
+        "Fonts can be customized.",
+        "Feel hap",
+        "Feel free ot ta",
+        "Feel free to take a look!",
+        "Feel free to take a look!",
+      ], // Strings to display
+      // Speed settings, try diffrent values untill you get good results
+      startDelay: 300,
+      typeSpeed: 100,
+      backSpeed: 40,
+      backDelay: 100,
+      cursorChar: "|",
+      smartBackspace: true,
+    });
+
+    // Destropying
+    return () => {
+      typed.destroy();
+    };
+  }, [isSSR]);
+
+  if (isSSR)
+    return (
       <WelcomeTextStyle>
-        {isSSR ? (
-          <h1 id="home-placeholder">
-            Hello world! I am Sangkara Welcome to my portfolio website. Fonts
-            can be customized. Feel free to take a look!
-          </h1>
-        ) : (
-          <TypeWriterEffect
-            startDelay={1000}
-            cursorColor="white"
-            multiText={[
-              "Hello world!",
-              "I am Sangkara.",
-              "Welcome to my portfolio website.",
-              "Fonts can be customized.",
-              "Feel free to take a look!",
-            ]}
-            multiTextDelay={1000}
-            typeSpeed={120}
-          />
-        )}
+        <h1 id="home-placeholder">
+          Hello world! I am Sangkara Welcome to my portfolio website. Fonts can
+          be customized. Feel free to take a look!
+        </h1>
+        <span className="typed1"></span>
       </WelcomeTextStyle>
-    </>
+    );
+  return (
+    <WelcomeTextStyle>
+      <span className="typed1"></span>
+    </WelcomeTextStyle>
   );
 };
 
