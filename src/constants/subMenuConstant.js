220d0b2fcb52b0e8fca7d5c03f9daa14b66ce4eb
astabidasaaa <astabidasabcd@gmail.com>
2021-12-28 02:07:04 +0700

    sub menu independent file

---
+++ src/constants/subMenuConstant.js
@@ -0,0 +1,37 @@
+export const menuTools = [
+  {
+    title: "Tools",
+    url: "/tools",
+  },
+  {
+    title: "Calculator",
+    url: "/tools/calculator",
+  },
+  {
+    title: "Timer",
+    url: "/tools/timer",
+  },
+  {
+    title: "Drum Machine",
+    url: "/tools/drum-machine",
+  },
+  {
+    title: "To Do List",
+    url: "/tools/to-do-list",
+  },
+];
+
+export const menuVisualization = [
+  {
+    title: "Visualization",
+    url: "/visualization",
+  },
+  {
+    title: "ID Map",
+    url: "/visualization/ID-map",
+  },
+  {
+    title: "US Map",
+    url: "/visualization/US-map",
+  },
+];
