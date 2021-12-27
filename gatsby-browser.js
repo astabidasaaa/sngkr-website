7784842460eb964e7f2a8d6ee21e0a09d9fa6500
astabidasaaa <astabidasabcd@gmail.com>
2021-12-28 02:04:19 +0700

    add framer motion to dependency

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Layout from "./src/components/Layout";
 
+// export function onRouteUpdate({ location, prevLocation }) {
+//   console.log("new pathname", location.pathname);
+//   console.log("old pathname", prevLocation ? prevLocation.pathname : null);
+// }
+
 export function wrapPageElement({ element, props }) {
   return <Layout {...props}>{element}</Layout>;
 }
